1d6293831312bdbae2e30f3134aee34ca2e0fe2c
Wander Costa <wanderfernando@gmail.com>
2019-02-17 19:09:32 -0300

    Adds ignore out folder and inserts a loop on main class execution

--- src/main/java/com/wfercosta/tw/assignment/train/Main.java
+++ src/main/java/com/wfercosta/tw/assignment/train/Main.java
@@ -1,29 +1,39 @@
 package com.wfercosta.tw.assignment.train;
 
+import com.wfercosta.tw.assignment.train.commons.exception.ApplicationException;
 import com.wfercosta.tw.assignment.train.router.model.Route;
 import com.wfercosta.tw.assignment.train.router.model.RoutePath;
 import com.wfercosta.tw.assignment.train.router.model.Trip;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Scanner;
 
 public class Main {
 
+    public static final Logger LOG = LoggerFactory.getLogger(Main.class);
     public static final String BLOCK_SPACE = "    ";
 
     public static void main (String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Inform the graph edges (e.g. AB9, AD5, BD1): ");
-        Router router = Router.withGraph(scanner.nextLine());
+        while (true) {
+            try {
+                System.out.println("Inform the graph edges (e.g. AB9, AD5, BD1): ");
+                Router router = Router.withGraph(scanner.nextLine());
 
 
-        System.out.println("Inform the route path (e.g. A, B, C):");
-        Trip trip = router.compute(RoutePath.withPath(scanner.nextLine()));
+                System.out.println("Inform the route path (e.g. A, B, C):");
+                Trip trip = router.compute(RoutePath.withPath(scanner.nextLine()));
 
-        printSelectedRoute(trip);
-        printShortestRoute(trip);
-        printPossibleRoutes(trip);
+                printSelectedRoute(trip);
+                printShortestRoute(trip);
+                printPossibleRoutes(trip);
+            } catch (ApplicationException e) {
+                LOG.error(e.getMessage());
+            }
+        }
 
     }
 
